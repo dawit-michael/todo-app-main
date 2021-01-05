a225368889d5cf2494b126f091953bc3dce15182
Dawit Michael <dave.mic1227@gmail.com>
2021-01-05 18:02:13 +0300

    version 1.0.0
    created basic elements
    along with  responsivness

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        BrightBlue: "#3a7bfd",
+        VeryLightGray: "	#fafafa",
+        VeryLightGrayishBlue: "#e4e5f1",
+        LightGrayishBlue: "#d2d3db",
+        DarkGrayishBlue: "#9394a5",
+        VeryDarkGrayishBlue: "#484b6a",
+        VeryDarkBlue: "#161722",
+        VeryDarkDesaturatedBlue: "#25273c",
+        LightGrayishBlue: "	#cacde8",
+        LightGrayishBluehover: "	#e4e5f1",
+        DarkGrayishBlue: "#777a92",
+        VeryDarkGrayishBlue: "	#4d5066",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
